chaule97/partner-contact

[MIG] partner_identification_unique_by_category: Migration to 18.0

partner_identification_unique_by_category/tests/test_partner_identification_unique_by_category.py
@@ -2,10 +2,11 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 from odoo.exceptions import ValidationError
-from odoo.tests.common import SavepointCase
+
+from odoo.addons.base.tests.common import SavepointCaseWithUserDemo
 
 
-class TestPartnerIdentificationUniqueByCategory(SavepointCase):
+class TestPartnerIdentificationUniqueByCategory(SavepointCaseWithUserDemo):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
@@ -67,7 +68,6 @@ class TestPartnerIdentificationUniqueByCategory(SavepointCase):
                 "partner_id": self.partner_1.id,
             }
         )
-        self.category_1.has_unique_numbers = True
         self.category_1.has_unique_numbers = False
         self.env["res.partner.id_number"].create(
             {

t-core-one/oca-partner-contact

partner_identification_unique_by_category/tests/test_partner_identification_unique_by_category.py
@@ -2,10 +2,11 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 from odoo.exceptions import ValidationError
-from odoo.tests.common import SavepointCase
+
+from odoo.addons.base.tests.common import BaseCommon
 
 
-class TestPartnerIdentificationUniqueByCategory(SavepointCase):
+class TestPartnerIdentificationUniqueByCategory(BaseCommon):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
@@ -67,7 +68,6 @@ class TestPartnerIdentificationUniqueByCategory(SavepointCase):
                 "partner_id": self.partner_1.id,
             }
         )
-        self.category_1.has_unique_numbers = True
         self.category_1.has_unique_numbers = False
         self.env["res.partner.id_number"].create(
             {